d85e15d546546fcec4912415bb8b1873dcddf701
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-08-25 13:44:28 +1200

    Reduce logging.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   def hello
-    Console.logger.info(self, params)
     render body: 'Hello'
   end
 end
--- preload.rb
+++ preload.rb
@@ -1,14 +1,2 @@
 
 require_relative "config/environment"
-
-module PrintRequestHeaders
-	def read_headers
-		headers = super
-		
-		Console.logger.warn(self, headers.fields)
-		
-		return headers
-	end
-end
-
-::Protocol::HTTP1::Connection.prepend(PrintRequestHeaders)
